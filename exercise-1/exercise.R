8e0c60e205623a8aa430ab7e76920c3bfe32d041
Joel Ross <joelross@uw.edu>
2017-01-30 00:36:47 -0800

    Update exercises for students

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,46 +1,33 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-library("httr")
-library("jsonlite")
+
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
 
 # For what years does the API have statistical data?
-response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
-body <- content(response,"text")
-years <- fromJSON(body)
-years
+
 
 # What is the "country code" for the "Syrian Arab Republic"?
-response <- GET("http://data.unhcr.org/api/countries/list.json")
-countries <- fromJSON(content(response, "text"))
-filter(countries, name_en == "Syrian Arab Republic") %>%
-  select(country_code)
+
 
 # How many persons of concern from Syria applied for residence in the USA in 2013?
 # Hint: you'll need to use a query parameter
 # Use the `str()` function to print the data of interest
 # See http://www.unhcr.org/en-us/who-we-help.html for details on these terms
-query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", year = 2013)
-response <- GET("http://data.unhcr.org/api/stats/persons_of_concern.json", query = query.params)
-usa.persons <- fromJSON(content(response,"text"))
-str(usa.persons)
+
 
 ## And this was only 2013...
 
 
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
-query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", population_type_code = "RF")
-response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json?", query = query.params)
-refugees.usa <- fromJSON(content(response,"text"))
-refugees.usa <- select(refugees.usa, year, usa = value)
+
 
 # Use the `plot()` function to plot the year vs. the value.
 # Add `type="o"` as a parameter to draw a line
-plot(refugees.usa, type="o")
+
 
 
 # Pick one other country in the world (e.g., Turkey).
@@ -48,15 +35,7 @@ plot(refugees.usa, type="o")
 # Is it more or less than the USA? (Hint: join the tables and add a new column!)
 # Hint: To compare the values, you'll need to convert the data (which is a string) to a number; try using `as.numeric()`
 
-query.params <- list(country_of_residence = "TUR", country_of_origin = "SYR", population_type_code = "RF")
-response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json?", query = query.params)
-refugees.other <- fromJSON(content(response,"text"))
-refugees.other <- select(refugees.other, year, other = value)
-refugees.other
 
-left_join(refugees.usa, refugees.other, by="year") %>%
-    mutate(usa = as.numeric(usa), other = as.numeric(other)) %>%
-    mutate(usa.more = usa > other)
 
 ## Bonus (not in solution):
 # How many of the refugees in 2013 were children (between 0 and 4 years old)?
@@ -66,12 +45,6 @@ left_join(refugees.usa, refugees.other, by="year") %>%
 # How many total people applied for asylum in the USA in 2013?
 # - You'll need to filter out NA values; try using `is.na()`
 # - To calculate a sum, you'll need to convert the data (which is a string) to a number; try using `as.numeric()`
-query.params <- list(country_of_asylum = "USA", year = 2013)
-response <- GET("http://data.unhcr.org/api/stats/asylum_seekers.json", query = query.params)
-usa.seekers <- fromJSON(content(response,"text"))
-select(usa.seekers, applied_during_year) %>%
-  filter(is.na(applied_during_year) == FALSE) %>%  # remove NA values
-  mutate(applied_during_year = as.numeric(applied_during_year)) %>%  # make numbers
-  summarize(sum(applied_during_year))
+
 
 ## Also note that asylum seekers are not refugees
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,11 +1,10 @@
 ### Exercise 2 ###
 
 # Load the httr and jsonlite libraries for accessing data
-library("httr")
-library("jsonlite")
+
 
 # Create a `base.uri` variable that holds the base uri. You wil then paste endpoints to this base.
-base.uri <- "https://api.spotify.com"
+
 
 
 ## As you may have noticed, it often takes multiple queries to retrieve the desired information.
@@ -15,58 +14,26 @@ base.uri <- "https://api.spotify.com"
 
 # Define a function `TopTrackSearch` that takes in an artist name as an argument,
 # and returns the top 10 tracks (in the US) by that artist
-TopTrackSearch <- function(artist) {
-  # Artist info
-  resource <- "/v1/search"
-  uri = paste0(base.uri, resource)
-  query.params <- list(q = artist, type = "artist")
 
-  response <- GET(uri, query = query.params)
 
-  artist.info <- fromJSON(content(response, "text"))
-  artist.id <- artist.info$artists$items$id[1]
 
-  # Get albums
-  resource <- paste0("/v1/artists/",artist.id,"/top-tracks")
-  uri = paste0(base.uri, resource)
-  query.params <- list(country="US")
-  response <- GET(uri, query = query.params)
-
-  top.tracks <- fromJSON(content(response, "text"))
-
-  return(top.tracks$tracks)
-}
 
 # What are the top 10 tracks by Beyonce?
-top.tracks <- TopTrackSearch('Beyonce')
+
 
 # Use the `flatten` function to flatten the data.frame -- note what differs!
-top.tracks <- flatten(top.tracks)
+
 
 # Use the `save()` function to save the flattened data frame to a file `beyonce.Rdata`
-save(top.tracks, file="beyonce.Rdata")
+
 
 # Use your `dplyr` functions to get the number of the songs that appear on each album
-num.album <- top.tracks %>%
-  group_by(album.name) %>%
-  summarise(n = n()) %>%
-  arrange(-n)
-num.album
+
 
 
 ### Bonus ###
 # Write a function that allows you to specify a search type (artist, album, etc.), and a string,
 # that returns the album/artist/etc. page of interest
-SpotifySearch <- function(type, string) {
 
-  resource <- "/v1/search"
-  uri = paste0(base.uri, resource)
-  query.params <- list(q = string, type = type)
-
-  response <- GET(uri, query = query.params)
-
-  return(flatten(fromJSON(content(response,"text"))))
-}
 
 # Search albums with the word "Sermon"
-SpotifySearch('album', 'Sermon')
