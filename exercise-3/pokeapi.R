c7faa36f5d3b15c9317abdddc531350079ca026b
Andrea Chen <abchen@uw.edu>
2017-01-31 03:01:15 -0800

    complete poke api

--- exercise-3/pokeapi.R
+++ exercise-3/pokeapi.R
@@ -1,5 +1,10 @@
+# packages only need to be installed once per dev machine
+#install.packages("jsonlite")
+#install.packages("httr")
+
 library(jsonlite)
 library(httr)
+library(dplyr)
 
 # Make a variable base.url that has the same base url from the poke api documentation.
 # (Hint: visit http://pokeapi.co/ to find the base url)
@@ -33,11 +38,55 @@ print(response$status_code)
 # what function did we use to extract the data from response?
 body <- content(response, "text")
 
-# create a variable parsed.data that will convert the JSON string into a list
-parsed.data <- fromJSON(body)
+# create a variable poke.data that will convert the JSON string into a list
+poke.data <- fromJSON(body)
 
-# Print out the keys from the parsed.data by using the names function
-print(names(parsed.data))
+# Print out the keys from the poke.data by using the names function
+print(names(poke.data))
+
+# Check if poke.data is a data frame. Print that value.
+print(is.data.frame(poke.data))
+
+# Check if moves in poke.data is a data frame. Moves is one of the many keys stored in this
+# JSON object. How would you call moves? Print that value.
+print(is.data.frame(poke.data$moves))
+
+# Food for thought
+# What do you think is the diffrence between the two outputs above? Why are they diffrent?
+
+# Create a data frame named poke.moves with the moves. be sure to use the head function to only get the
+# first few values. Feel free to use view to look at the dataframe.
+poke.moves <- head(poke.data$moves)
+#View(poke.moves)
+
+# Flatten the poke.moves data frame
+poke.moves <- flatten(poke.moves)
+
+# Use the colnames function to learn what columns are in your poke.moves data frame. Print this.
+print(colnames(poke.moves))
+
+# Using dplyr remove all columns except the one with your move names. 
+poke.moves <- select(poke.moves, move.name)
+
+# Using dplyr add one coloumn that indicates the type of the move (feel free to guess!) and one column that 
+# indicates on a scale of 1-5 how much you like that move, with 1 being not at all and 5 being it's my favorite. 
+poke.moves <- mutate(poke.moves, move.type = c("fire", "ice", "thunder", "flying", "normal", "flying"),
+                     move.rank = c(4, 5, 4, 3, 1, 2))
+
+# Using dplyr order the moves by rank. Higher ranking moves should be on the top of the dataframe. 
+poke.moves <- arrange(poke.moves, -move.rank)
+
+# Bonus 1
+# Download a sprite of your pokemon. Be sure to download it to your png folder. Name the downloaded file using
+# your pokemon's name. Be sure to check that there is a link to the sprite you chose.
+poke.img <- poke.data$sprites
+download.file(poke.img$front_default[1], destfile="png/dragonite.png", mode="wb")
+
+
+
+
+
+
 
 
 
